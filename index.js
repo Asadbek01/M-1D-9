2875b92f98a53e4fd89c0964b8079ff64681eb82
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2021-10-01 00:30:20 +0200

    updated

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-
-// const displayNumber = function () {
-
-//     const bingoContainerNode = document.getElementById("bingo-container ")
-
-//     for (let bingoNumber = 1; bingoNumber <= 76; bingoNumber++) {
-
-//         let numberNode = document.createElement("div")
-//         numberNode.classList.add("numbers")
-//         numberNode.innerText = bingoNumber
-
-//         bingoContainerNode.appendChild(numberNode)
-//     }
-
-// }
-// window.onload = function () {
-//     displayNumber()
-// }
-
-
 const displayNumber = function() {
 
 
@@ -40,7 +20,7 @@ const displayNumber = function() {
 window.onload = function() {
     displayNumber()
 
-    
+
 }
 
 
